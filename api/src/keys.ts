afd0e12fc4f914a91d6f217212100341770574fa
Daniel Aragon <laragon@bluemessaging.com>
2019-07-04 13:04:48 -0500

    Make env variable names homogenous. Add AWS EB Dockerrun file.

--- api/src/keys.ts
+++ api/src/keys.ts
@@ -1,16 +1,16 @@
 const keys = {
-  pgDatabase: process.env.PGDATABASE,
-  pgHost: process.env.PGHOST,
-  pgPassword: process.env.PGPASSWORD,
-  pgPort: parseInt(process.env.PGPORT ? process.env.PGPORT : '5432', 10),
-  pgUser: process.env.PGUSER,
+  pgDatabase: process.env.PG_DATABASE,
+  pgHost: process.env.PG_HOST,
+  pgPassword: process.env.PG_PASSWORD,
+  pgPort: parseInt(process.env.PG_PORT ? process.env.PG_PORT : '5432', 10),
+  pgUser: process.env.PG_USER,
   redisHost: process.env.REDIS_HOST,
   redisPort: parseInt(
     process.env.REDIS_PORT ? process.env.REDIS_PORT : '6379',
     10,
   ),
   serverPort: parseInt(
-    process.env.SERVER_PORT ? process.env.SERVER_PORT : '5000',
+    process.env.API_PORT ? process.env.API_PORT : '5000',
     10,
   ),
 }
